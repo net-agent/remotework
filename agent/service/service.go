ad86daeede648ba004863a48a3c59217d7bd8a2f
gzshi <vip@eval.im>
2021-08-03 18:43:04 +0800

    optimize color impliment

--- agent/service/service.go
+++ agent/service/service.go
@@ -26,8 +26,3 @@ func NewService(mnet *agent.MixNet, info agent.ServiceInfo) Service {
 	}
 	return nil
 }
-
-func color(color int, info string) string { return fmt.Sprintf("\x1b[%dm%v\x1b[0m", color, info) }
-func green(info string) string            { return color(32, info) }
-func red(info string) string              { return color(31, info) }
-func yellow(info string) string           { return color(33, info) }
