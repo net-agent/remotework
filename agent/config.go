4d912ea791b0e3d98942c565e2c72d9fdd7960c6
unknown <lyhfr123@126.com>
2022-03-29 19:46:04 +0800

    更新配置结构体中toml标签

--- agent/config.go
+++ agent/config.go
@@ -38,28 +38,28 @@ func NewConfig(configFileName string) (*Config, error) {
 }
 
 type ServerInfo struct {
-	Listen   string `json:"listen"`   // 监听的地址
-	Password string `json:"password"` // 校验连接的密码
-	WsEnable bool   `json:"wsEnable"` // 是否启用Websocket
-	WsPath   string `json:"wsPath"`   // Websocket路径
+	Listen   string `json:"listen" toml:"listen"`     // 监听的地址
+	Password string `json:"password" toml:"password"` // 校验连接的密码
+	WsEnable bool   `json:"wsEnable" toml:"wsEnable"` // 是否启用Websocket
+	WsPath   string `json:"wsPath" toml:"wsPath"`     // Websocket路径
 }
 
 type AgentInfo struct {
-	Enable     bool   `json:"enable"`
-	Network    string `json:"network"`  // 网络名称，不能为tcp、tcp4、tcp6
-	Address    string `json:"address"`  // 服务端地址
-	Password   string `json:"password"` // 连接服务的密码
-	Domain     string `json:"domain"`   // 独立域名（不能重复）
-	URL        string `json:"url"`      // <Network>://<Domain>:<Password>@<Address>
-	WsEnable   bool   `json:"wsEnable"` // 是否为Websocket服务
-	Wss        bool   `json:"wss"`      // 是否为wss协议
-	WsPath     string `json:"wsPath"`   // Websocket路径
-	QuickTrust Trust  `json:"trust"`
+	Enable     bool   `json:"enable" toml:"enable"`
+	Network    string `json:"network" toml:"network"`   // 网络名称，不能为tcp、tcp4、tcp6
+	Address    string `json:"address" toml:"address"`   // 服务端地址
+	Password   string `json:"password" toml:"password"` // 连接服务的密码
+	Domain     string `json:"domain" toml:"domain"`     // 独立域名（不能重复）
+	URL        string `json:"url" toml:"url"`           // <Network>://<Domain>:<Password>@<Address>
+	WsEnable   bool   `json:"wsEnable" toml:"wsEnable"` // 是否为Websocket服务
+	Wss        bool   `json:"wss" toml:"wss"`           // 是否为wss协议
+	WsPath     string `json:"wsPath" toml:"wsPath"`     // Websocket路径
+	QuickTrust Trust  `json:"trust" toml:"trust"`
 }
 
 type Trust struct {
-	Enable    bool              `json:"enable"`
-	WhiteList map[string]string `json:"whiteList"`
+	Enable    bool              `json:"enable" toml:"enable"`
+	WhiteList map[string]string `json:"whiteList" toml:"whiteList"`
 }
 
 func (agent *AgentInfo) GetConnectFn() ConnectFunc {
@@ -140,27 +140,27 @@ func (agent *AgentInfo) getTcpConnectFn(mac string) ConnectFunc {
 }
 
 type PortproxyInfo struct {
-	ListenURL string `json:"listen"`
-	TargetURL string `json:"target"`
-	LogName   string `json:"log"`
+	ListenURL string `json:"listen" toml:"listen"`
+	TargetURL string `json:"target" toml:"target"`
+	LogName   string `json:"log" toml:"log"`
 }
 
 type Socks5Info struct {
-	ListenURL string `json:"listen"`
-	Username  string `json:"username"`
-	Password  string `json:"password"`
-	LogName   string `json:"log"`
+	ListenURL string `json:"listen" toml:"listen"`
+	Username  string `json:"username" toml:"username"`
+	Password  string `json:"password" toml:"password"`
+	LogName   string `json:"log" toml:"password"`
 }
 
 type QuickVisitInfo struct {
-	ListenURL string `json:"listen"`
-	TargetURL string `json:"target"`
-	LogName   string `json:"log"`
+	ListenURL string `json:"listen" toml:"listen"`
+	TargetURL string `json:"target" toml:"target"`
+	LogName   string `json:"log" toml:"log"`
 }
 
 type RDPInfo struct {
-	ListenURL string `json:"listen"`
-	LogName   string `json:"log"`
+	ListenURL string `json:"listen" toml:"listen"`
+	LogName   string `json:"log" toml:"log"`
 }
 
 func getMacAddr() ([]string, error) {
